cd0c06cfe22e8bc84537b2dd3f2f635474c05438
HongQin <hqinlab@gmail.com>
2015-10-02 09:17:44 -0400

    changed to 3 groups: Young, mid, old

--- 20151002.R
+++ 20151002.R
@@ -1,12 +1,5 @@
 # Version info: R 2.14.1, Biobase 2.15.3, GEOquery 2.23.2, limma 3.10.1
-# R scripts generated  Tue Sep 29 08:22:41 EDT 2015
-# source("http://bioconductor.org/biocLite.R")
-# biocLite()
-# biocLite("Biobase")
-# biocLite( "GEOquery")
-# biocLite("limma")
-
-rm(list=ls())
+# R scripts generated  Fri Oct 2 09:16:15 EDT 2015
 
 ################################################################
 #   Differential expression analysis with limma
@@ -14,34 +7,26 @@ library(Biobase)
 library(GEOquery)
 library(limma)
 
-setwd("~/github/student.project.archives/spelman/2015-2016/FL")
+# load series and platform data from GEO
 
-# load series and platform data from GEO, 
-# This takes a few minutes
-gset <- getGEO("GSE32719", GSEMatrix =TRUE)[[1]]
-# see http://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE32719
-
-# load NCBI platform annotation
-gpl <- annotation(gset)
-platf <- getGEO(gpl, AnnotGPL=TRUE)
-ncbifd <- data.frame(attr(dataTable(platf), "table"))
+gset <- getGEO("GSE32719", GSEMatrix =TRUE)
+if (length(gset) > 1) idx <- grep("GPL570", attr(gset, "names")) else idx <- 1
+gset <- gset[[idx]]
 
 # make proper column names to match toptable 
 fvarLabels(gset) <- make.names(fvarLabels(gset))
 
 # group names for all samples
-sml <- c("Gu","Gn","Gd","Ge","Gf","Gi","Gn","Ge","Gd");
+sml <- c("G0","G0","G0","G0","G0","G0","G0","G0","G0","G0","G0","G0","G0","G0","G1","G1","G1","G1","G1","G2","G2","G2","G2","G2","G2","G2","G2");
 
 # log2 transform
 ex <- exprs(gset)
 qx <- as.numeric(quantile(ex, c(0., 0.25, 0.5, 0.75, 0.99, 1.0), na.rm=T))
 LogC <- (qx[5] > 100) ||
   (qx[6]-qx[1] > 50 && qx[2] > 0) ||
-  (qx[2] > 0 && qx[2] < 1 && qx[4] > 1 && qx[4] < 2)  #some kind of error checks
-if (LogC) { 
-  ex[which(ex <= 0)] <- NaN
-  exprs(gset) <- log2(ex) 
-}
+  (qx[2] > 0 && qx[2] < 1 && qx[4] > 1 && qx[4] < 2)
+if (LogC) { ex[which(ex <= 0)] <- NaN
+exprs(gset) <- log2(ex) }
 
 # set up the data and proceed with analysis
 fl <- as.factor(sml)
@@ -49,18 +34,22 @@ gset$description <- fl
 design <- model.matrix(~ description + 0, gset)
 colnames(design) <- levels(fl)
 fit <- lmFit(gset, design)
-cont.matrix <- makeContrasts(Gu-Gd, Ge-Gd, Gf-Ge, Gi-Gf, Gn-Gi, Gu-Gn, levels=design)
+cont.matrix <- makeContrasts(G2-G0, G1-G0, G2-G1, levels=design)
 fit2 <- contrasts.fit(fit, cont.matrix)
 fit2 <- eBayes(fit2, 0.01)
 tT <- topTable(fit2, adjust="fdr", sort.by="B", number=250)
 
+# load NCBI platform annotation
+gpl <- annotation(gset)
+platf <- getGEO(gpl, AnnotGPL=TRUE)
+ncbifd <- data.frame(attr(dataTable(platf), "table"))
 
 # replace original platform annotation
 tT <- tT[setdiff(colnames(tT), setdiff(fvarLabels(gset), "ID"))]
 tT <- merge(tT, ncbifd, by="ID")
 tT <- tT[order(tT$P.Value), ]  # restore correct order
 
-tT <- subset(tT, select=c("ID","adj.P.Val","P.Value","F"))
+tT <- subset(tT, select=c("ID","adj.P.Val","P.Value","F","Gene.symbol","Gene.title"))
 write.table(tT, file=stdout(), row.names=F, sep="\t")
 
 ################################################################
@@ -69,22 +58,24 @@ library(Biobase)
 library(GEOquery)
 
 # load series and platform data from GEO
-gset <- getGEO("GSE32719", GSEMatrix =TRUE)[[1]]
+
+gset <- getGEO("GSE32719", GSEMatrix =TRUE)
+if (length(gset) > 1) idx <- grep("GPL570", attr(gset, "names")) else idx <- 1
+gset <- gset[[idx]]
 
 # group names for all samples in a series
-sml <- c("Gu","Gn","Gd","Ge","Gf","Gi","Gn","Ge","Gd")
+sml <- c("G0","G0","G0","G0","G0","G0","G0","G0","G0","G0","G0","G0","G0","G0","G1","G1","G1","G1","G1","G2","G2","G2","G2","G2","G2","G2","G2")
 
 # order samples by group
 ex <- exprs(gset)[ , order(sml)]
 sml <- sml[order(sml)]
 fl <- as.factor(sml)
-labels <- c("undefined")
+labels <- c("young","mid","old")
 
 # set parameters and draw the plot
-palette(c("#undefined", "#AABBCC"))
+palette(c("#f4dfdf","#dfeaf4","#f2cb98", "#AABBCC"))
 dev.new(width=4+dim(gset)[[2]]/5, height=6)
 par(mar=c(2+round(max(nchar(sampleNames(gset)))/2),4,2,1))
 title <- paste ("GSE32719", '/', annotation(gset), " selected samples", sep ='')
 boxplot(ex, boxwex=0.6, notch=T, main=title, outline=FALSE, las=2, col=fl)
 legend("topleft", labels, fill=palette(), bty="n")
-
